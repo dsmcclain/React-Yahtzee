1f81570b40762402f0b5bf08ff5a9b8151873ac1
Dylan McClain <dylan.mcclain@gmail.com>
2019-01-06 15:38:20 -0800

    return hold state to Dice.js, add styles to indicate dice with hold state

--- src/Dice.js
+++ src/Dice.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
-import "./Dice.css";
 
 // import images of dice faces
 import one from './images/one.png';
@@ -10,12 +9,27 @@ import four from './images/four.png';
 import five from './images/five.png';
 import six from './images/six.png';
 
-function Dice(props) {
-	const images = [one, two, three, four, five, six]
-	return (
-		<div className="dice-container">
-			<img src={images[props.pips]} />
-		</div>
-	);
+class Dice extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hold: false,
+		}
+		this.holdDice = this.holdDice.bind(this);
+	}
+
+	holdDice() {
+		this.setState({ hold: !this.state.hold })
+		console.log('state is now ' + this.state.hold);
+	}
+
+	render() {
+		const images = [one, two, three, four, five, six]
+		return (
+			<div className={`dice-container ${this.state.hold ? "hold" : "free"}`}>
+				<img src={images[this.props.pips]} onClick={this.holdDice} />
+			</div>
+		);
+	}
 }
 export default hot(module)(Dice);
--- src/DiceBar.js
+++ src/DiceBar.js
@@ -7,11 +7,11 @@ class DiceBar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			diceA: {pips: 0, hold: false},
-			diceB: {pips: 0, hold: false},
-			diceC: {pips: 0, hold: false},
-			diceD: {pips: 0, hold: false},
-			diceE: {pips: 0, hold: false},
+			pipsA: 0,
+			pipsB: 0,
+			pipsC: 0,
+			pipsD: 0,
+			pipsE: 0,
 		}
 		this.clickHandler = this.clickHandler.bind(this);
 	}
@@ -25,11 +25,11 @@ class DiceBar extends Component {
 		const rollE = Math.floor(Math.random() * 6);
 
 		this.setState({
-			diceA: {pips: rollA},
-			diceB: {pips: rollB},
-			diceC: {pips: rollC},
-			diceD: {pips: rollD},
-			diceE: {pips: rollE},
+			pipsA: rollA,
+			pipsB: rollB,
+			pipsC: rollC,
+			pipsD: rollD,
+			pipsE: rollE,
 		});
 	}
 
@@ -40,11 +40,11 @@ class DiceBar extends Component {
 							className="roll-button"
 							onClick={this.clickHandler}>Roll!</button>
 				<div className="dice-bar">
-					<Dice pips={this.state.diceA.pips} />
-					<Dice pips={this.state.diceB.pips} />
-					<Dice pips={this.state.diceC.pips} />
-					<Dice pips={this.state.diceD.pips} />
-					<Dice pips={this.state.diceE.pips} />
+					<Dice pips={this.state.pipsA} />
+					<Dice pips={this.state.pipsB} />
+					<Dice pips={this.state.pipsC} />
+					<Dice pips={this.state.pipsD} />
+					<Dice pips={this.state.pipsE} />
 				</div>
 			</div>
 		);
