dcf924da081e539740b35e1f36fdfbd8b4c625b8
Dylan McClain <dylan.mcclain@gmail.com>
2019-01-31 09:59:07 -0800

    finish building scorecard

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
+import { isNull } from "util";
 
 class ScoreTable extends Component {
   render() {
@@ -65,12 +66,27 @@ class Scorecard extends Component {
 const UPPER_ITEMS = [
   {name: 'Aces', description: 'Total of all Aces', score: 3},
   {name: 'Twos', description: 'Total of all Twos', score: 6},
-  {name: 'Threes', description: 'Total of all Threes', score: 9}
+  {name: 'Threes', description: 'Total of all Threes', score: 9},
+  {name: 'Fours', description: 'Total of all Threes', score: 9},
+  {name: 'Fives', description: 'Total of all Threes', score: 9},
+  {name: 'Sixes', description: 'Total of all Threes', score: 9},
+  {name: 'Subtotal', description: 'Total of all Scores', score: 9},
+  {name: 'Bonus (If Subtotal > 63', description: 'Score 35', score: 35, fulfilled: false},
+  {name: 'Total', description: 'Subtotal + Bonus', score: 9},
 ];
 
 const LOWER_ITEMS = [
-  {name: 'Full House', description: 'Two of a Kind + Three of a Kind', score: 25, fulfilled: false},
-  {name: 'YAHTZEE', description: 'All Five Dice the Same', score: 50, fulfilled: false},
+  {name: 'Three of a Kind', description: 'Total of All Dice'},
+  {name: 'Four of a Kind)', description: 'Total of All Dice'},
+  {name: 'Full House', description: 'Score 25', score: 25, fulfilled: false},
+  {name: 'Small Straight (Sequence of 4)', description: 'Score 30', score: 50, fulfilled: false},
+  {name: 'Large Straight (Sequence of 5)', description: 'Score 40', score: 50, fulfilled: false},
+  {name: 'YAHTZEE', description: 'Score 50', score: 50, fulfilled: false},
+  {name: 'Chance', description: 'Total of All Dice', score: isNull},
+  {name: 'YAHTZEE Bonus (Additional YAHTZEEs)', description: 'Score 100', score: 100, fulfilled: false},
+  {name: 'Upper Total', description: ' ⇒ ', score: isNull},
+  {name: 'Lower Total', description: ' ⇒ ', score: isNull},
+  {name: 'Grand Total', description: 'Upper Total + Lower Total', score: isNull},
 ];
 
 
