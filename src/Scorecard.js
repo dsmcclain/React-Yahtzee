03cfe792cdc27a0f6b884ba2ac74faea1e1d12cf
Dylan McClain <dylan.mcclain@gmail.com>
2019-02-01 09:07:50 -0800

    more scorecard styling

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
-import { isNull } from "util";
+import { isNull, isNullOrUndefined } from "util";
 import "./Scorecard.css";
 
 class ScoreTable extends Component {
@@ -65,29 +65,29 @@ class Scorecard extends Component {
 }
 
 const UPPER_ITEMS = [
-  {name: 'Aces', description: 'Total of all Aces', score: 3},
-  {name: 'Twos', description: 'Total of all Twos', score: 6},
-  {name: 'Threes', description: 'Total of all Threes', score: 9},
-  {name: 'Fours', description: 'Total of all Threes', score: 9},
-  {name: 'Fives', description: 'Total of all Threes', score: 9},
-  {name: 'Sixes', description: 'Total of all Threes', score: 9},
-  {name: 'Subtotal', description: 'Total of all Scores', score: 9},
-  {name: 'Bonus (If Subtotal > 63', description: 'Score 35', score: 35, fulfilled: false},
-  {name: 'Total', description: 'Subtotal + Bonus', score: 9},
+  {name: 'Aces', description: 'Total of all Aces', score: isNullOrUndefined},
+  {name: 'Twos', description: 'Total of all Twos', score: isNullOrUndefined},
+  {name: 'Threes', description: 'Total of all Threes', score: isNullOrUndefined},
+  {name: 'Fours', description: 'Total of all Threes', score: isNullOrUndefined},
+  {name: 'Fives', description: 'Total of all Threes', score: isNullOrUndefined},
+  {name: 'Sixes', description: 'Total of all Threes', score: isNullOrUndefined},
+  {name: 'Subtotal', description: 'Total of all Scores', score: isNullOrUndefined},
+  {name: 'Bonus (If Subtotal > 63)', description: 'Score 35', score: 35, fulfilled: false},
+  {name: 'Total', description: 'Subtotal + Bonus', score: isNullOrUndefined},
 ];
 
 const LOWER_ITEMS = [
-  {name: 'Three of a Kind', description: 'Total of All Dice'},
-  {name: 'Four of a Kind)', description: 'Total of All Dice'},
+  {name: 'Three of a Kind', description: 'Total of All Dice', score: isNullOrUndefined},
+  {name: 'Four of a Kind)', description: 'Total of All Dice', score: isNullOrUndefined},
   {name: 'Full House', description: 'Score 25', score: 25, fulfilled: false},
   {name: 'Small Straight (Sequence of 4)', description: 'Score 30', score: 50, fulfilled: false},
   {name: 'Large Straight (Sequence of 5)', description: 'Score 40', score: 50, fulfilled: false},
   {name: 'YAHTZEE', description: 'Score 50', score: 50, fulfilled: false},
-  {name: 'Chance', description: 'Total of All Dice', score: isNull},
+  {name: 'Chance', description: 'Total of All Dice', score: isNullOrUndefined},
   {name: 'YAHTZEE Bonus (Additional YAHTZEEs)', description: 'Score 100', score: 100, fulfilled: false},
-  {name: 'Upper Total', description: ' ⇒ ', score: isNull},
-  {name: 'Lower Total', description: ' ⇒ ', score: isNull},
-  {name: 'Grand Total', description: 'Upper Total + Lower Total', score: isNull},
+  {name: 'Upper Total', description: ' ⇒ ', score: isNullOrUndefined},
+  {name: 'Lower Total', description: ' ⇒ ', score: isNullOrUndefined},
+  {name: 'Grand Total', description: 'Upper Total + Lower Total', score: isNullOrUndefined},
 ];
 
 
