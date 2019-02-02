8572d86b208fcd56a862cd6f265686e4b62dcbfd
Dylan McClain <dylan.mcclain@gmail.com>
2019-02-01 19:28:44 -0800

    create cells for all upper section fields

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,19 +8,32 @@ class Calculator extends Component {
 		this.state = {
 			acesActive: false,
 			acesTotal: 0,
+			twosActive: false,
+			twosTotal: 0,
+			threesActive: false,
+			threesTotal: 0,
+			foursActive: false,
+			foursTotal: 0,
+			fivesActive: false,
+			fivesTotal: 0,
+			sixesActive: false,
+			sixesTotal: 0,
 		}
-		this.checkAces = this.checkAces.bind(this);
 	}
 
 	componentDidUpdate(prevProps) {
 		if (this.props.pips !== prevProps.pips) {
 			console.log('check aces');
 			this.checkAces(this.props.pips);
+			this.checkTwos(this.props.pips);
+			this.checkThrees(this.props.pips);
+			this.checkFours(this.props.pips);
+			this.checkFives(this.props.pips);
+			this.checkSixes(this.props.pips);
 		}
 	}
 
 	checkAces(pips) {
-		console.log(pips);
 		let count = pips.filter(function(x){return x==0}).length;
 		pips.includes(0) ? 
 			this.setState({
@@ -32,9 +45,76 @@ class Calculator extends Component {
 			})
 	}
 
+	checkTwos(pips) {
+		let count = pips.filter(function(x){return x==1}).length;
+		pips.includes(1) ? 
+			this.setState({
+				twosActive: true,
+				twosTotal: count*2,
+			}) : 
+			this.setState({
+				twosActive: false,
+			})
+	}
+
+	checkThrees(pips) {
+		let count = pips.filter(function(x){return x==2}).length;
+		pips.includes(2) ? 
+			this.setState({
+				threesActive: true,
+				threesTotal: count*3,
+			}) : 
+			this.setState({
+				threesActive: false,
+			})
+	}
+
+	checkFours(pips) {
+		let count = pips.filter(function(x){return x==3}).length;
+		pips.includes(3) ? 
+			this.setState({
+				foursActive: true,
+				foursTotal: count*4,
+			}) : 
+			this.setState({
+				foursActive: false,
+			})
+	}
+
+	checkFives(pips) {
+		let count = pips.filter(function(x){return x==4}).length;
+		pips.includes(4) ? 
+			this.setState({
+				fivesActive: true,
+				fivesTotal: count*5,
+			}) : 
+			this.setState({
+				fivesActive: false,
+			})
+	}
+
+	checkSixes(pips) {
+		let count = pips.filter(function(x){return x==5}).length;
+		pips.includes(5) ? 
+			this.setState({
+				sixesActive: true,
+				sixesTotal: count*6,
+			}) : 
+			this.setState({
+				sixesActive: false,
+			})
+	}
+
 	render() {
 		return (
-			<Cell eligible={this.state.acesActive} suggestion={this.state.acesTotal} />
+			<div>
+				<Cell eligible={this.state.acesActive} suggestion={this.state.acesTotal} />
+				<Cell eligible={this.state.twosActive} suggestion={this.state.twosTotal} />
+				<Cell eligible={this.state.threesActive} suggestion={this.state.threesTotal} />
+				<Cell eligible={this.state.foursActive} suggestion={this.state.foursTotal} />
+				<Cell eligible={this.state.fivesActive} suggestion={this.state.fivesTotal} />
+				<Cell eligible={this.state.sixesActive} suggestion={this.state.sixesTotal} />
+			</div>
 		)
 	}
 }
